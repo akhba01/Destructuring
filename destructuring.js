40a1982bb277c2d264f0ef8f52decd718a433134
akhba01 <aakhbar21@gmail.com>
2022-05-10 13:04:34 +0700

    default parameter

--- destructuring.js
+++ destructuring.js
@@ -1,12 +1,14 @@
 const user = {
-    name: 'John',
-    age: 30,
-    country : 'USA',
-    links : [{
-        github : 'github.com/john',
-        facebook : 'facebook.com/john'
-    }]
-}
+  name: "John",
+  age: 30,
+  country: "USA",
+  links: [
+    {
+      github: "github.com/john",
+      facebook: "facebook.com/john",
+    },
+  ],
+};
 
 // conventional way
 var name = user.name;
@@ -14,19 +16,42 @@ var age = user.age;
 var country = user.country;
 
 // destructuring way
-var { name: userName ='guest', age, country} = user;
+var { name: userName = "guest", age, country } = user;
 console.log(`${userName} is ${age} years old and lives in ${country}`);
 // const { links: { github, facebook } } = user;
 // console.log(`${github} and ${facebook}`);
 
-const { links : [{github, facebook}]} = user;
+const {
+  links: [{ github, facebook }],
+} = user;
 console.log(github, facebook);
 
 // function greeting(user){
 //     return `Hello ${user.name}`;
 // }
-function greeting({name}){
-    return `Hello ${name}`;
+function greeting({ name }) {
+  return `Hello ${name}`;
 }
 
 console.log(greeting(user));
+
+// ______________________________________________________________________________
+function marker({
+  name = "Uknown",
+  location: { lat, lng } = {
+    lat: 0,
+    lng: 0,
+  },
+}) {
+  console.log(`${name} is at ${lat}, ${lng}`);
+}
+
+const bogor = {
+  name: "Bogor",
+  location: {
+    lat: -6.5795,
+    lng: 106.8294,
+  },
+};
+
+marker(bogor);
